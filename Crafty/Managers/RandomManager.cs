8ff0bc902bc43cb897d0ac2fdb1978dbd857cd65
heapy <momonster1999@gmail.com>
2023-06-12 12:09:04 +0200

    Avalonia changes

--- Crafty/Managers/RandomManager.cs
+++ Crafty/Managers/RandomManager.cs
@@ -10,9 +10,8 @@ namespace Crafty.Managers
 	{
 		public static async Task<Bitmap> RandomCover()
 		{
-			IAssetLoader assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
 			Uri uri = new Uri($"avares://Crafty/Assets/background-{new Random().Next(1, 4)}.png");
-			return new Bitmap(assets.Open(uri));
+			return new Bitmap(AssetLoader.Open(uri));
 		}
 	}
 }
